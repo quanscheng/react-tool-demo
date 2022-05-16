c16ac07378ad0728a790e3474bb2e9d4720b65b3
quanscheng <quanscheng@163.com>
2022-05-16 21:29:32 +0800

    demo

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className='App'>
       <div className={styles.demoWraper}>this is the css module demo</div>
+      <button className={styles.button}>标签选择器</button>
     </div>
   )
 }
